f82880124adb00652304d1735522461ba70be06b
SherryH <enjoy.wind7rain@gmail.com>
2017-01-18 23:04:32 -0800

    Changed the models to be Mongoose models

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,32 @@
-var db = require('../config');
-var crypto = require('crypto');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = new Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: {type: Number, default: 0},
+  //date: { type: Date, default: Date.now },
 });
+
+var Link = mongoose.model('Link', linkSchema);
+// var db = require('../config');
+// var crypto = require('crypto');
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function() {
+//     this.on('creating', function(model, attrs, options) {
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
 
 module.exports = Link;
